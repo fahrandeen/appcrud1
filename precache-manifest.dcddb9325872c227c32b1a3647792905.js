fb913e53877b268a235e208dbf67a1257cde9bad
Fahran Deen <fahrandeen17@gmail.com>
2020-02-11 12:55:19 +0530

    Updates

---
+++ precache-manifest.dcddb9325872c227c32b1a3647792905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5672bec36c719666bec06efe697f8e00",
+    "url": "/appcrud200207/index.html"
+  },
+  {
+    "revision": "ae9f866218861dce8bdc",
+    "url": "/appcrud200207/static/css/main.9fa12f07.chunk.css"
+  },
+  {
+    "revision": "45e0c18228a4d75443f2",
+    "url": "/appcrud200207/static/js/2.982e28d9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/appcrud200207/static/js/2.982e28d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9f866218861dce8bdc",
+    "url": "/appcrud200207/static/js/main.6e74151f.chunk.js"
+  },
+  {
+    "revision": "dffe3131f8b69b8e063f",
+    "url": "/appcrud200207/static/js/runtime-main.f1a35cc4.js"
+  }
+]);
